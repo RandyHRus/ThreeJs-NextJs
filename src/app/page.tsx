a7e7cf48f8e7c053d9c8df004305f0398dbfe00b
RandyHRus <25328677+RandyHRus@users.noreply.github.com>
2024-01-01 23:31:21 -0800

    ThreeJs-NextJs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+"use client";
+import { Alert, Snackbar } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [camera, setCamera] = useState<THREE.PerspectiveCamera | null>(null);
+    const [renderer, setRenderer] = useState<THREE.WebGLRenderer | null>(null);
+    const [error, setError] = useState<string>("");
+
+    // It is laggy to resize the window, especially in a 3D scene. Use throttling to optimize. More info:
+    // https://web.archive.org/web/20220714020647/https://bencentra.com/code/2015/02/27/optimizing-window-resize.html
+    let throttledResize = useRef<boolean>(false);
+    const throttleResizeDelay: number = 250;
+
+    const handleErrorClose = () => {
+        console.log("closing error");
+        setError("");
+    };
+
+    // Handle page load
+    useEffect(() => {
+        const cameraStart = new THREE.Vector3(0, 0, 5);
+        // Create a Three.js camera
+        let _camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+        setCamera(_camera);
+        _camera.position.set(cameraStart.x, cameraStart.y, cameraStart.z);
+
+        // Create a Three.js renderer
+        const canvas: HTMLElement = document.getElementById(
+            "threeCanvas"
+        ) as HTMLElement;
+        try {
+            const _renderer = new THREE.WebGLRenderer({
+                canvas,
+                antialias: true,
+            });
+            _renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(_renderer.domElement);
+            setRenderer(_renderer);
+        } catch (err) {
+            let errorMsg: string =
+                "Failed to render WebGL scene. This could be due to hardware acceleration being turned off in your browser settings.";
+            console.error(errorMsg);
+            setError(errorMsg);
+        }
+    }, []);
+
+    // Initialize scene
+    useEffect(() => {
+        const resizeCanvas = () => {
+            if (!camera || !renderer) return;
+
+            if (!throttledResize.current) {
+                // we're throttled!
+                throttledResize.current = true;
+                // set a timeout to un-throttle
+                setTimeout(function () {
+                    camera.aspect = window.innerWidth / window.innerHeight;
+                    camera.updateProjectionMatrix();
+                    renderer.setSize(window.innerWidth, window.innerHeight);
+                    throttledResize.current = false;
+                }, throttleResizeDelay);
+            }
+        };
+
+        if (!camera || !renderer) return;
+
+        // Create a Three.js scene
+        const scene = new THREE.Scene();
+
+        // Create a cube
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        //add light
+        const light = new THREE.PointLight(0xffffff, 1, 100);
+        light.position.set(0, 0, 0);
+        scene.add(light);
+
+        // Add animation and update the scene
+        const animate = () => {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        };
+
+        animate();
+
+        // Add event listener to handle resizing
+        window.addEventListener("resize", resizeCanvas);
+
+        // Return a cleanup function to dispose of Three.js resources
+        return () => {
+            renderer.dispose();
+            document.body.removeChild(renderer.domElement);
+            window.removeEventListener("resize", resizeCanvas);
+        };
+    }, [camera, renderer]);
+
+    return (
+        <div>
+            <div className="fixed bg-tertiary h-screen w-full z-10">
+                <canvas
+                    id="threeCanvas"
+                    style={{
+                        position: "fixed",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                    }}
+                />
+            </div>
+            <Snackbar
+                open={error !== ""}
+                autoHideDuration={60000}
+                onClose={handleErrorClose}
+                className="z-10"
+            >
+                <Alert severity="error" sx={{ width: "100%" }}>
+                    {error}
+                </Alert>
+            </Snackbar>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
